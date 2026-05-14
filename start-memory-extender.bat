683dc0d22a1bf985fa5053b166f6276ed32eb09b
TC Lowe <s272122@nwmissouri.edu>
2026-05-14 17:15:15 -0500

    fix: set working directory explicitly in start bat so .env loads correctly

--- start-memory-extender.bat
+++ start-memory-extender.bat
@@ -1,5 +1,4 @@
 @echo off
 echo Starting Memory Extender...
-cd /d "%~dp0memory-extender"
-start "Memory Extender" cmd /k "npm run dev"
+start "Memory Extender" /d "%~dp0memory-extender" cmd /k "npm run dev"
 echo Memory Extender started. Close the "Memory Extender" window to stop it.

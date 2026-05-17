68a3054bce9323f8869816689e2f2218db02e782
TC Lowe <s272122@nwmissouri.edu>
2026-05-17 14:02:28 -0500

    fix: bump regex install key to v3 to force reinstall with remember pattern

--- marinara-extender.js
+++ marinara-extender.js
@@ -13,7 +13,7 @@
 const MEMORY_EXTENDER = "http://127.0.0.1:3001";
 const ME_DEBUG = true;   // set false to silence pipeline tracing
 const dbg = (...a) => ME_DEBUG && console.debug("[ME:dbg]", ...a);
-const REGEX_INSTALLED_KEY = `${marinara.extensionId}:regex-installed:v2`;
+const REGEX_INSTALLED_KEY = `${marinara.extensionId}:regex-installed:v3`;
 const REGEX_SCRIPT_NAME = "Marinara Extender: Strip memory tags";
 
 const REGEX_MANIFEST = {
